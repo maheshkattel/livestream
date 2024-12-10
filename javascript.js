0a5b384487be9db52b7856d6cf187b1f0f82e91a
Mahesh Kattel <maheshjammarcattel@gmail.com>
2024-12-10 15:04:18 +0545

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,7 +6,8 @@ class Mahesh {
             projectId: "live--link",
             storageBucket: "live--link.appspot.com",
             messagingSenderId: "72803483857",
-            appId: "1:72803483857:android:3e32e5a6138d8324c6c5f0"
+            appId: "1:72803483857:android:3e32e5a6138d8324c6c5f0",
+            saudiProLeague:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fsaudileague%2Fsaudileague.png?alt=media&token=35f00f1a-df3e-4922-a248-b18e503751bc"
         };
     }
 }
