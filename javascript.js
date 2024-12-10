1101f3c978b43d65c708c89169ba97c3d1453829
Mahesh Kattel <maheshjammarcattel@gmail.com>
2024-12-10 15:22:03 +0545

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -7,7 +7,19 @@ class Mahesh {
             storageBucket: "live--link.appspot.com",
             messagingSenderId: "72803483857",
             appId: "1:72803483857:android:3e32e5a6138d8324c6c5f0",
-            saudiProLeague:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fsaudileague%2Fsaudileague.png?alt=media&token=35f00f1a-df3e-4922-a248-b18e503751bc"
+            saudiProLeague:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fsaudileague%2Fsaudileague.png?alt=media&token=35f00f1a-df3e-4922-a248-b18e503751bc",
+            allMatches:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fall_matches%2Fall_matches.png?alt=media&token=740e4c5b-ca52-40c3-a58c-3f63dcbd138d",
+            bundesliga:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fbundesliga%2Fbundesliga.png?alt=media&token=ccd4fd33-e907-4bd5-8b96-d3c223990dd3",
+            championsLeague:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fchampionsleague%2Fchampionsleague.png?alt=media&token=0be570ae-a4d3-46cf-9f4b-63d8b85a6696",
+            europaLeague:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Feuropaleague%2Feuropaleague.png?alt=media&token=9cbff90c-e448-416e-975c-02183a9f62cb",
+            ipl:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fipl%2Fipl.png?alt=media&token=26e2af46-c295-407e-a121-1006e790eb51",
+            laliga:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Flaliga%2Flaliga.png?alt=media&token=94c4c124-fccb-438d-aa6f-3f78ff78fcc0",
+            ligue1:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fligue1%2Fligue1.png?alt=media&token=3a013e51-56e1-4765-92a6-a756917151ec",
+            mls:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fmls%2Fmls.png?alt=media&token=a1fee902-dd24-4fb1-993d-02efd389d5e2",
+            npl:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fnpl%2Fnpl.webp?alt=media&token=bf8dc71c-6a11-4986-b4f2-9c5ca1b36d6b",
+            premierLeague:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fpremierleague%2Fpremierleague.png?alt=media&token=22cc4dbb-ff60-4682-9ccb-5edc9e152bef",
+            serieA:"https://firebasestorage.googleapis.com/v0/b/live--link.appspot.com/o/logos%2Fseriea%2Fseriea.png?alt=media&token=239c6631-c7a3-43aa-b95d-a55c3d401096",
+        
         };
     }
 }
